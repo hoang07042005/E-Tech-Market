c93f81abdd1398933b249a933d7f0c424ae7a9bf
hoang07042005 <142959468+hoang07042005@users.noreply.github.com>
2026-06-19 16:13:19 +0700

    update

--- e-tech-market-backend/app/Http/Controllers/Client/SseController.php
+++ e-tech-market-backend/app/Http/Controllers/Client/SseController.php
@@ -29,6 +29,13 @@ class SseController extends Controller
             flush();
 
             try {
+                if (php_sapi_name() === 'cli-server' && strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+                    echo "data: {\"type\": \"ping\"}\n\n";
+                    if (ob_get_level() > 0) ob_flush();
+                    flush();
+                    return; // Return immediately to free up the thread. Client will wait 3s due to 'retry: 3000'
+                }
+
                 $redis->subscribe([$channel], function ($message) {
                     echo "data: " . $message . "\n\n";
                     if (ob_get_level() > 0) {
